a5d67be613d3c8dd97a95f010f033158fb3c343e
Gnaarf <gnaarf@gmx.de>
2018-05-20 01:37:03 +0200

    Handles on ColorAligner

--- Assets/Editor/ColorAlignerEditor.cs
+++ Assets/Editor/ColorAlignerEditor.cs
@@ -6,13 +6,37 @@ using UnityEngine;
 [CustomEditor(typeof(ColorAligner))]
 public class ColorAlignerEditor : Editor
 {
+
+    Vector3 pos = Vector3.zero;
+    Quaternion rot = Quaternion.identity;
+
     public void OnSceneGUI()
     {
         ColorAligner myTarget = (ColorAligner)target;
 
         foreach (ColorAligner.ColorVector2 cv in myTarget.AlignVectors)
         {
-            cv.Vector = Handles.PositionHandle(myTarget.transform.position + (Vector3)cv.Vector.normalized, Quaternion.identity) - myTarget.transform.position;
+            //cv.Vector = Handles.PositionHandle(myTarget.transform.position + (Vector3)cv.Vector.normalized, Quaternion.identity) - myTarget.transform.position;
+
+            //Vector3 pos = myTarget.transform.position + (Vector3)cv.Vector.normalized;
+          //Quaternion rot = Quaternion.identity;
+
+
+
+
+            Handles.color = cv.Color;
+
+            var handlePos = myTarget.transform.position + (Vector3)cv.Vector;
+
+            float size = HandleUtility.GetHandleSize(myTarget.transform.position) * 0.06f;
+            Vector3 snap = Vector3.one * 0.5f;
+
+            EditorGUI.BeginChangeCheck();
+            Vector3 newPos = Handles.FreeMoveHandle(handlePos, Quaternion.identity, size, snap, Handles.CircleHandleCap);
+            if (EditorGUI.EndChangeCheck())
+            {
+                cv.Vector = (newPos - myTarget.transform.position).normalized;
+            }
         }
     }
 }
